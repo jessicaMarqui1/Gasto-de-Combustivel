d3b4b932cc09e439d1e330bb2e9938563b80acde
jessicaMarqui1 <jessicamarquirocha95@gmail.com>
2023-03-07 20:52:58 +0900

    Project's Initial Commit

---
+++ www/components/motor.js
@@ -0,0 +1,15 @@
+
+function olamundo() {
+    alert('Olá Mundo')
+}
+ 
+ function calcularconsumo(){
+     var km, gas, consumo;
+
+     km =  document.getElementById('km').value 
+     gas = document.getElementById('gas').value
+     consumo = km / gas
+
+     alert(consumo)
+
+ }
